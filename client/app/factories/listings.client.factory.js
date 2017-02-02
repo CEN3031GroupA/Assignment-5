67035f21e07678b074856ce5cee5e9ca0f9f6760
[Amy Ly] <[amyly@ufl.edu]>
2017-02-01 20:46:15 -0500

    serve to HTTPS instead of HTTP

--- client/app/factories/listings.client.factory.js
+++ client/app/factories/listings.client.factory.js
@@ -2,23 +2,23 @@ angular.module('listings').factory('Listings', ['$http',
   function($http) {
     var methods = {
       getAll: function() {
-        return $http.get('http://localhost:80/api/listings');
+        return $http.get('https://localhost:80/api/listings');
       },
 
       create: function(listing) {
-        return $http.post('http://localhost:80/api/listings', listing);
+        return $http.post('https://localhost:80/api/listings', listing);
       }, 
 
       read: function(id) {
-        return $http.get('http://localhost:80/api/listings/' + id);
+        return $http.get('https://localhost:80/api/listings/' + id);
       }, 
 
       update: function(id, listing) {
-        return $http.put('http://localhost:80/api/listings/' + id, listing);
+        return $http.put('https://localhost:80/api/listings/' + id, listing);
       }, 
 
       delete: function(id) {
-        return $http.delete('http://localhost:80/api/listings/' + id);
+        return $http.delete('https://localhost:80/api/listings/' + id);
       }
     };
 
